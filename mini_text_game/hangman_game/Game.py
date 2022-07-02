ac239188fafb84c511fe4f23c038becacca36146
Yipei Zhao <zhaoyipei99@gmail.com>
2022-07-02 22:38:43 +0100

    updated
    mostly work, need to determine the success of each guess

--- mini_text_game/hangman_game/Game.py
+++ mini_text_game/hangman_game/Game.py
@@ -1,20 +1,39 @@
 import json
+import string
 import random
 with open('dictionary.json') as f:
 	words = json.load(f)
 f.close()
 
 while True:
+	flag = True
 	word = random.choice(list(words.keys()))
 	word_def = words[word]
-	print("You have 8 hearts left. ♥♥♥♥ ♥♥♥♥")
-	hidden = ""
-	for i in range(len(word)):
-		hidden += "_ "
-	print(hidden)
 	print(word_def)
-	print("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z")
-	
+	heart = 8
+	print("You have " + str(heart) + " hearts left." + "♥"*heart)
+	charas = [string.ascii_uppercase]
+
+	# Happens in one round/guess
+	while flag:
+		hidden = ["_"]*len(word)
+		print(" ".join(hidden))
+		print("/".join(charas))
+		guess = input("Your guess guess is:")
+		index = []
+		for i in range(len(word)):
+			if word[i]==guess:
+				index.append(i)
+		if len(index)!=0:
+			print("The letter " + guess + " is in the word.")
+			for i in index:
+				hidden[i] = guess
+		else:
+			print("The letter " + guess + " is not in the word.")
+			heart-=1
+		print("You have " + str(heart) + " hearts left." + "♥"*heart)
+		print(" ".join(hidden))
+
 
 	# Game start
 	# Set score
